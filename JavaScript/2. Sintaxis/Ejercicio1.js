fd200f10b753992228377072dc2d3c95471f8e51
Jefry Avila <jeff23avila@gmail.com>
2022-07-24 20:42:30 -0600

    Ejercicio 1
    Lista con diferente información.

---
+++ JavaScript/2. Sintaxis/Ejercicio1.js
@@ -0,0 +1,12 @@
+var lista = [
+    "Jefry", 
+    26, 
+    true, 
+    new Date(1996, 3, 23), 
+    {
+        titulo: "The Lost Symbol", 
+        autor:"Dan Brown", 
+        fecha: new Date(2009, 8, 15), 
+        url:"https://www.penguinrandomhouse.com/books/19314/the-lost-symbol-by-dan-brown/"
+    }
+]
