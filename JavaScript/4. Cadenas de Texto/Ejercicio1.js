3850d160c3e5bf7ccdc1faaeabae906ade58579b
Jefry Avila <jeff23avila@gmail.com>
2022-07-27 22:09:55 -0600

    Ejercicio1
    Manejo de cadenas.

---
+++ JavaScript/4. Cadenas de Texto/Ejercicio1.js
@@ -0,0 +1,12 @@
+let nombre = 'Jefry';
+let apellido = 'Ávila'
+let estudiante = nombre.concat(' ').concat(apellido);
+let estudianteMayus = estudiante.toUpperCase;
+let estudianteMinus = estudiante.toLowerCase;
+let estudianteTamanio = estudiante.length;
+let primeroLetraNombre = nombre.charAt(0);
+let ultimaLetraApellido = apellido.charAt(apellido.length - 1);
+let estudianteSinEspacios = estudiante.replace(/ /g, '');
+let nombreEnEstudiante = estudiante.includes('Jefry');
+
+
