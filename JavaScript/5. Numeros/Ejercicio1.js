34ea005a4534cb32fd9feefae7ffb2109f24c675
Jefry Avila <jeff23avila@gmail.com>
2022-07-31 21:52:24 -0600

    Ejercicio 1
    Manejo de números en JS.

---
+++ JavaScript/5. Numeros/Ejercicio1.js
@@ -0,0 +1,11 @@
+const altura_cm = 178
+const altura_m = 1.78
+const peso_kg = 75.0
+
+const altura_redondeadad = Math.ceil(altura_m);
+console.log('altura_redondeadad :>> ', altura_redondeadad);
+const peso_redondeado = Math.floor(peso_kg);
+console.log('peso_redondeado :>> ', peso_redondeado);
+
+const esIgual = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+console.log('esIgual :>> ', esIgual);
