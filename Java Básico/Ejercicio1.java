96541588b58029da1794ac9be79ecbbd4b27458a
Jefry Avila <jeff23avila@gmail.com>
2022-09-12 16:20:18 -0600

    Ejercicio1
    Tipos de datos en Java

---
+++ Java Básico/Ejercicio1.java
@@ -0,0 +1,29 @@
+package com.ejercicios;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        // enteros
+        byte number1 = 1;
+        short number2 = 10;
+        int number3 = 40;
+        long number4 = 1090;
+
+        // decimales
+        float decimal1 = 8.7f;
+        double decimal2 = 6.66d;
+
+        // cadena de texto
+        String nombre = "Jefry";
+        String apellido = "Ávila";
+
+        // caracter
+        char caracter1 = 'a';
+
+        //booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+    }
+
+}
